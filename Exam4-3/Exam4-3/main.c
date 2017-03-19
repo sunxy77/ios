5c5bbb350f9626da6b9655a770db0506b296ab19
孙晓晔 <sunxy@sunxiaoyematomac-mini.local>
2017-03-19 12:49:02 +0800

    C语言实现一个动态数组

---
+++ Exam4-3/Exam4-3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Exam4-3
+//
+//  Created by 孙晓晔 on 2017/3/19.
+//  Copyright © 2017年 孙晓晔. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
