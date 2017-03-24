43ae8bf9a17d5cdd71245f80e9fa11da96184c2a
孙晓晔 <sunxy@sunxiaoyematomac-mini.local>
2017-03-24 21:04:03 +0800

    finish

---
+++ Exam6-3/Exam6-3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Exam6-3
+//
+//  Created by 孙晓晔 on 2017/3/24.
+//  Copyright © 2017年 孙晓晔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Exam6-3/Exam6-3/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Exam6-3
+//
+//  Created by 孙晓晔 on 2017/3/24.
+//  Copyright © 2017年 孙晓晔. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
