30253f3d56ee9e75a13b37ff4758b0a62a662f8c
孙晓晔 <sunxiaoye@sunxy.local>
2017-03-26 10:30:14 +0800

    initial

---
+++ Example7-2/Example7-2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example7-2
+//
+//  Created by 孙晓晔 on 2017/3/26.
+//  Copyright © 2017年 孙晓晔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example7-2/Example7-2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Example7-2
+//
+//  Created by 孙晓晔 on 2017/3/26.
+//  Copyright © 2017年 孙晓晔. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
