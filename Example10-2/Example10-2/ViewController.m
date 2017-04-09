d63e4c2e50e0c1d29feb6009dc3b013988404c0b
孙晓晔 <sunxy@sunxiaoyematomac-mini.local>
2017-04-09 17:51:28 +0800

    完善

--- Example10-2/Example10-2/ViewController.m
+++ Example10-2/Example10-2/ViewController.m
@@ -9,8 +9,16 @@
 #import "ViewController.h"
 #import <AVFoundation/AVFoundation.h>
 
+#define ItemStatus @"status"
+
 @interface ViewController ()
 @property(nonatomic,strong) AVPlayer *player1;
+@property (weak, nonatomic) IBOutlet UIButton *playBtn;
+@property (weak, nonatomic) IBOutlet UIButton *pauseBtn;
+@property (weak, nonatomic) IBOutlet UIButton *stopBtn;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UIProgressView *progress;
+
 @property(nonatomic,strong) AVPlayerLayer *playLayer;
 @end
 
@@ -22,6 +30,65 @@
     
 //    [self.view.layer addSublayer:self.playLayer];
     [self playLayer];
+    
+    [self.player1.currentItem addObserver:self forKeyPath:ItemStatus options:NSKeyValueObservingOptionNew context:nil];
+    
+    [self.player1.currentItem addObserver:self forKeyPath:@"loadedTimeRanges" options:NSKeyValueObservingOptionNew context:nil];
+    
+}
+- (IBAction)playcurVideo:(UISlider *)sender {
+    [self.player1.currentItem seekToTime:CMTimeMake(self.slider.value, 1) completionHandler:^(BOOL finished) {
+        [self.player1 play];
+    }];
+    
+}
+
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    
+    if ([keyPath isEqualToString:ItemStatus]) {
+        AVPlayerItem *item = object;
+        
+        if (item.status == AVPlayerItemStatusReadyToPlay) {
+            
+            long long  llsecond = item.duration.value / item.duration.timescale;
+            
+            self.slider.minimumValue = 0;
+            self.slider.maximumValue = llsecond;
+            
+            __weak typeof (self) weakSelf = self;
+            
+            [self.player1 addPeriodicTimeObserverForInterval:CMTimeMake(1, 1) queue:NULL usingBlock:^(CMTime time) {
+                weakSelf.slider.value = weakSelf.player1.currentItem.currentTime.value / weakSelf.player1.currentItem.currentTime.timescale;
+            }];
+            
+            NSLog(@"AVPlayerItemStatusReadyToPlay");
+            self.playBtn.enabled = TRUE;
+        } else if(item.status == AVPlayerItemStatusFailed) {
+            NSLog(@"AVPlayerItemStatusFailed");
+        } else if(item.status == AVPlayerItemStatusUnknown) {
+            
+            NSLog(@"AVPlayerItemStatusUnknown");
+        }
+    } else if ([keyPath isEqualToString:@"loadedTimeRanges"]) {
+        AVPlayerItem *item = object;
+        
+        long long  llTotalSecond = item.duration.value / item.duration.timescale;
+        
+        
+        NSValue *value = [item.loadedTimeRanges lastObject];
+        CMTimeRange range = [value CMTimeRangeValue];
+        long long llsecond = range.start.value / range.start.timescale + range.duration.value / range.duration.timescale;
+        
+        [self.progress setProgress:((float)llsecond/llTotalSecond) animated:YES];
+        
+        
+    }
+}
+
+-(void)dealloc {
+
+    [self.player1.currentItem removeObserver:self forKeyPath:ItemStatus];
+    [self.player1.currentItem removeObserver:self forKeyPath:@"loadedTimeRanges"];
 }
 
 
@@ -53,8 +120,8 @@
         
         _playLayer.anchorPoint = CGPointMake(0, 0);
         _playLayer.position = CGPointMake(0, 200);
-//        _playLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
-        _playLayer.videoGravity = AVLayerVideoGravityResize;
+        _playLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+//        _playLayer.videoGravity = AVLayerVideoGravityResize;
         
         [self.view.layer addSublayer:_playLayer];
         
